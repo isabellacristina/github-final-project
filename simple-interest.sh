c5270c4f3b3f85089f61268c5e0792a6ad172516
Isabella Soares <86267681+isabellacristina@users.noreply.github.com>
2023-10-17 23:27:31 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,7 +5,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <isabellacristina>
+   # isabellacristina
    
    # Input:
    # p, principal amount
